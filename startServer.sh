561271adfa65a9d77726e4c57f78bb52cddd9204
Pranomvignesh <pranomvignesh@gmail.com>
2020-09-16 11:54:58 +0530

    modified html

--- startServer.sh
+++ startServer.sh
@@ -1,3 +1,3 @@
-cp -r ./node_modules/monaco-editor/min ./public/monaco/
-cp -r ./node_modules/monaco-editor/min-maps ./public/monaco/
+cp -r ./node_modules/monaco-editor/min ./public/monaco-editor/
+cp -r ./node_modules/monaco-editor/min-maps ./public/monaco-editor/
 ./node_modules/nodemon/bin/nodemon.js server.js
